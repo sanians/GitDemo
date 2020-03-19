459d71b6ded93a37ea153551df9044bffa857dc5
sanians <1025912929@qq.com>
2020-03-19 15:04:29 +0800

    初始化

---
+++ app/src/main/java/com/example/wxy/gitdemo/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.wxy.gitdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //init 初始化了
+    }
+}
